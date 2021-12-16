4bc918bd1681a0532ee403a031766467b1e20ab9
Mel-louie <mdesfont@student.42.fr>
2021-12-16 12:42:32 +0100

    crontab push

--- bash/backup42cursusGit.sh
+++ bash/backup42cursusGit.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+find ~/42cursus -name "cronlog" -type f -mtime +2 -delete
 git -C ~/42cursus add .
 git -C ~/42cursus commit -m "auto commit"
 git -C ~/42cursus push
+echo "[`date`] modifications of 42cursus has been push on github" >> ~/42cursus/cronlog
