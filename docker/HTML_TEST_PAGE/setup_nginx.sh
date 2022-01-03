60baf11c8199e612fbc2ac22080c029a6f2c111d
Mel-louie <mdesfont@student.42.fr>
2022-01-03 15:20:20 +0100

    stack ok

--- docker/HTML_TEST_PAGE/setup_nginx.sh
+++ docker/HTML_TEST_PAGE/setup_nginx.sh
@@ -7,11 +7,11 @@ openssl req -newkey rsa:4096				\
 	-out ${CERT_PATH}		\
 	-keyout ${KEY_PATH}	\
 	-subj "/C=FR/CN=${DOMAIN_NAME}"
-sed -i "s~CERT_TEMPLATE~${CERT_PATH}~g" /etc/nginx/nginx.conf
-sed -i "s~KEY_TEMPLATE~${KEY_PATH}~g" /etc/nginx/nginx.conf
-sed -i "s/TLS_VERSION_TEMPLATE/${TLS_VERSION}/g" /etc/nginx/nginx.conf
-sed -i "s/SERVER_NAME_TEMPLATE/${DOMAIN_NAME}/g" /etc/nginx/nginx.conf
-sed -i "s/PHP_FPM_HOST_TEMPLATE/${PHP_FPM_HOST}/g" /etc/nginx/nginx.conf
-sed -i "s/PHP_FPM_PORT_TEMPLATE/${PHP_FPM_PORT}/g" /etc/nginx/nginx.conf
+sed -i "s~CERT_TEMPLATE~${CERT_PATH}~g" /etc/nginx.conf
+sed -i "s~KEY_TEMPLATE~${KEY_PATH}~g" /etc/nginx.conf
+sed -i "s/TLS_VERSION_TEMPLATE/${TLS_VERSION}/g" /etc/nginx.conf
+sed -i "s/SERVER_NAME_TEMPLATE/${DOMAIN_NAME}/g" /etc/nginx.conf
+sed -i "s/PHP_FPM_HOST_TEMPLATE/${PHP_FPM_HOST}/g" /etc/nginx.conf
+sed -i "s/PHP_FPM_PORT_TEMPLATE/${PHP_FPM_PORT}/g" /etc/nginx.conf
 
 nginx -g "daemon off;"
